340e064f9a5d3690a6afef8374f39aa9cfc49e62
Маша <masha.kustova-guselnikova@yandex.ru>
2020-05-21 12:06:27 +0500

    Добавляет стилизацию страницы каталога в трех версиях

--- source/js/catalog.js
+++ source/js/catalog.js
@@ -1,10 +1,12 @@
+var body = document.querySelector("body");
 var header = document.querySelector(".page-header");
 var buttonMenu = document.querySelector(".page-header__button");
 var buttonFilter = document.querySelector(".filter__button");
 var filter = document.querySelector(".filter");
 var closeFilter = document.querySelector(".filter__popup-button");
+var choiceButton = document.querySelectorAll(".choice__button");
 
-header.classList.remove("page-header--nojs");
+body.classList.remove("nojs");
 
 header.classList.remove("page-header--opened");
 
@@ -40,4 +42,17 @@ buttonFilter.addEventListener("click", function () {
 closeFilter.addEventListener("click", function () {
     filter.classList.remove("filter--opened");
     filter.classList.add("filter--closed");
+  });
+
+
+  choiceButton.forEach(function(item)  {
+    item.addEventListener("click", function (evt) {
+      evt.preventDefault();
+      var choiceContainer = item.parentNode.parentNode;
+      if(choiceContainer.classList.contains("choice__container--closed")) {
+        choiceContainer.classList.remove("choice__container--closed")
+      }else{
+        choiceContainer.classList.add("choice__container--closed")
+      }
+    });
   });
